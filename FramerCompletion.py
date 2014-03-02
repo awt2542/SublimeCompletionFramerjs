76fe1b3b2f0f423c080b916a5309f1393c3128ef
Andreas Wahlstrom <andreas.wahlstrom@tv4.se>
2014-03-02 17:19:55 +0100

    clean up

--- FramerCompletion.py
+++ FramerCompletion.py
@@ -2,29 +2,22 @@ import sublime, sublime_plugin, os, json, re
 from os.path import basename
 
 class FramerCompletionCommand(sublime_plugin.EventListener):
+	docCompletions, completions = [], [] # empty lists to save completions in
 
-	def is_supported_format(self, view):
-		if ".js" in view.file_name() or ".html" in view.file_name():
+	def is_supported_format(self, fileName):
+		if ".js" in fileName or ".html" in fileName:
 			return True
-
-	docCompletions = [] # list to save doc completion tuples in
-	completions = [] # list to save completion tuples in
-
-	dirname = os.path.dirname(os.path.abspath(__file__))
-	json_file = open(os.path.join(dirname,"framerdocs.json"))
-	try:
+	
+	# Add Framer docs
+	pluginFolder = os.path.dirname(os.path.abspath(__file__))
+	with open(os.path.join(pluginFolder,"framerdocs.json")) as json_file:
 		json_data = json.load(json_file)
 		for key in json_data:
 			docCompletions.append((key,json_data[key]))
-	except Exception, e:
-		pass
-	else:
-		json_file.close()
 
-	def findViews(self, path):
-		try:
-			file_lines = open(path)
-			self.completions[:] = [] # clear the list to refresh
+	def findViews(self, viewfile):
+		with open(viewfile) as file_lines:
+			self.completions[:] = [] # clear to refresh list
 			for line in file_lines:
 				if "name" in line:
 					pattern = r'"([^"]*)"' # inside quotes
@@ -32,21 +25,16 @@ class FramerCompletionCommand(sublime_plugin.EventListener):
 					view = "PSD[\""+viewname+"\"]"
 					self.completions.append((view+'\t'+"Framer",view)) #append a tuple with text to display and insert
 			self.completions.sort() # alphabetical order
-		except Exception, e:
-			pass
-		else:
-			file_lines.close()
 
 	def on_activated(self, view):
 		if view.file_name() is not None: # only run on saved files
 			pathToFile = os.path.dirname(view.file_name())
 			if os.path.exists(os.path.join(pathToFile,"framer/framer.js")): # a framer project
 				projectName = pathToFile.split("/")[-1]
-				viewsPath = pathToFile+"/framer/views."+projectName+".js"
-				if self.is_supported_format(view):
-					self.findViews(viewsPath)
-		
+				viewfile = pathToFile+"/framer/views."+projectName+".js"
+				if self.is_supported_format(view.file_name()):
+					self.findViews(viewfile)
 
 	def on_query_completions(self, view, prefix, locations):
-		if self.is_supported_format(view):
+		if self.is_supported_format(view.file_name()):
 			return self.completions + self.docCompletions
