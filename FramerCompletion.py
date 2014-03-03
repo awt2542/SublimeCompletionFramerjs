fe640bd4844efce08cb5c0febb942a9541a0c89f
Andreas Wahlstrom <andreas.wahlstrom@tv4.se>
2014-03-03 09:01:30 +0100

    dry refactoring

--- FramerCompletion.py
+++ FramerCompletion.py
@@ -1,23 +1,35 @@
 import sublime, sublime_plugin, os, json, re
 from os.path import basename
 
-class FramerCompletionCommand(sublime_plugin.EventListener):
+class pathInfo:
+	def __init__(self, path):
+		self.path = path
+		self.project_path = os.path.dirname(self.path)
+		self.project_name = self.project_path.split("/")[-1]
+		self.framer_path = self.project_path+"/framer"
+		self.view_file = self.framer_path+"/views."+self.project_name+".js"
+	
+class FramerCompletion(sublime_plugin.EventListener):
 	settings = sublime.load_settings('FramerCompletion.sublime-settings')
 
 	docCompletions, completions = [], [] # empty lists to save completions in
 
-	def is_supported_format(self, fileName):
-		if ".js" in fileName or ".html" in fileName:
-			return True
-	
 	# Add Framer docs
 	pluginFolder = os.path.dirname(os.path.abspath(__file__))
-	with open(os.path.join(pluginFolder,"framerdocs.json")) as json_file:
+	with open(os.path.join(pluginFolder,"framerdocs.json")) as json_file: #add this to class?
 		json_data = json.load(json_file)
 		for key in json_data:
 			docCompletions.append((key,json_data[key]))
 
-	def findViews(self, viewfile):
+	def is_supported_file(self, filePath):
+		if filePath is not None: # only run on saved files
+			if os.path.isdir(pathInfo(filePath).framer_path):
+				fileExtension = os.path.splitext(filePath)[1]
+				if fileExtension == ".js" or ".html":
+					return True
+
+	def findViews(self, filePath):
+		viewfile = pathInfo(filePath).view_file
 		with open(viewfile) as file_lines:
 			dotNotation = self.settings.get('dotNotation')
 			self.completions[:] = [] # clear to refresh list
@@ -33,14 +45,9 @@ class FramerCompletionCommand(sublime_plugin.EventListener):
 			self.completions.sort() # alphabetical order
 
 	def on_activated(self, view):
-		if view.file_name() is not None: # only run on saved files
-			pathToFile = os.path.dirname(view.file_name())
-			if os.path.exists(os.path.join(pathToFile,"framer/framer.js")): # a framer project
-				projectName = pathToFile.split("/")[-1]
-				viewfile = pathToFile+"/framer/views."+projectName+".js"
-				if self.is_supported_format(view.file_name()):
-					self.findViews(viewfile)
+		if self.is_supported_file(view.file_name()):
+			self.findViews(view.file_name())
 
 	def on_query_completions(self, view, prefix, locations):
-		if self.is_supported_format(view.file_name()):
+		if self.is_supported_file(view.file_name()):
 			return self.completions + self.docCompletions
